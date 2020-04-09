6ee776e6bea281bc238dda912989d1fe8a3b0777
Artem Pavliuk <art256100@gmail.com>
2020-04-09 11:50:32 +0300

    Tried vuex

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>{{postsCount}}</h1>
+        <PostForm/>
+        <div class="post" v-for="post in validPosts" :key="post.id">
+            <h2>{{post.title}}</h2>
+            <p>{{post.body}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import {mapGetters, mapActions} from 'vuex';
+    import PostForm from './components/PostForm';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        computed: mapGetters(['allPosts', 'postsCount', 'validPosts']),
+        methods: mapActions(['fetchPosts']),
+        components: {PostForm},
+        async mounted() {
+            this.fetchPosts();
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    .post {
+        border: 1px solid black;
+        margin-top: 20px;
+    }
 </style>
